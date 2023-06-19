ad2b76c7ba071fb5bf07f76c36e4d5a341527ecc
Nishi0517 <nishitadaru@gmail.com>
2023-06-19 12:46:41 +0530

    core api only no view & front end

---
+++ ContactAPI/Models/AddContactRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactAPI.Models
+{
+    public class AddContactRequest
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
